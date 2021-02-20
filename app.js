b0cfbfa9b996c0516c7b028b1e7ad6d886341131
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-02-20 17:12:17 +0000

    trabajo listo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
-let arrNuevo = [];
+let final = ['.com', '.net', '.us', '.io']
 
 
 
@@ -11,12 +11,9 @@ for (i = 0; i < pronoun.length; i++) {
     for (j = 0; j < adj.length; j++) {
 
         for (n = 0; n < noun.length; n++) {
-            arrNuevo = (pronoun[i] + adj[j] + noun[n]);
-            let arrSeparado = arrNuevo.split("");
-            let ultimas = arrSeparado[arrSeparado.length - 2] + arrSeparado[arrSeparado.length - 1];
-            if (ultimas ==1) {
-                console.log(anteUltima, ultima)
+            for (p = 0; p < final.length; p++) {
+                console.log(pronoun[i] + adj[j] + noun[n] + final[p]);
             }
         }
     }
-}
+}//falta el servidor nada mas
